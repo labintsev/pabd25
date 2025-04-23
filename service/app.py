d366d5fb927ee68d2f9846e5c65aafd9d18fab62
labintsev <andrej.labintsev@yandex.ru>
2025-04-23 20:23:45 +0300

    move to service dir

--- service/app.py
+++ service/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
@@ -12,6 +13,10 @@ def index():
 def process_numbers():
     # Здесь можно добавить обработку полученных чисел
     # Для примера просто возвращаем их обратно
+    data = request.get_json()
+    
+    print(data['number1'])
+    
     return {'status': 'success', 'data': 'Числа успешно обработаны'}
 
 if __name__ == '__main__':
