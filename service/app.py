62621124b163b97408cc3d76fe0dcaad80e04741
labintsev <andrej.labintsev@yandex.ru>
2025-05-07 19:55:09 +0300

    add black formatter

--- service/app.py
+++ service/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request 
+import argparse
+from flask import Flask, render_template, request
 from logging.config import dictConfig
 
 dictConfig(
@@ -14,7 +15,7 @@ dictConfig(
                 "class": "logging.StreamHandler",
                 "stream": "ext://sys.stdout",
                 "formatter": "default",
-            },         
+            },
             "file": {
                 "class": "logging.FileHandler",
                 "filename": "service/flask.log",
@@ -28,31 +29,38 @@ dictConfig(
 app = Flask(__name__)
 
 import joblib
-# Сохранение модели
-model_path = 'models/linear_regression_model.pkl'
 
-loaded_model = joblib.load(model_path)
+# Сохранение модели
+MODEL_NAME = "models/linear_regression_v1.pkl"
 
 
 # Маршрут для отображения формы
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # Маршрут для обработки данных формы
-@app.route('/api/numbers', methods=['POST'])
+@app.route("/api/numbers", methods=["POST"])
 def process_numbers():
 
     data = request.get_json()
-    
-    app.logger.info(f'Requst data: {data}')
-    try: 
-        area = float(data['area']) 
-        price = loaded_model.predict([[area]])[0]
-        price = int(price)
-    except  ValueError:
-        return {'status': 'error', 'data': 'Ошибка парсинга данных'}
-    return {'status': 'success', 'data': price }
 
-if __name__ == '__main__':
+    app.logger.info(f"Requst data: {data}")
+    try:
+        area = float(data["area"])
+        price = app.config["model"].predict([[area]])[0]
+        price = int(price)
+    except ValueError:
+        return {"status": "error", "data": "Ошибка парсинга данных"}
+    return {"status": "success", "data": price}
+
+
+if __name__ == "__main__":
+    """Parse arguments and run lifecycle steps"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", help="Model name", default=MODEL_NAME)
+    args = parser.parse_args()
+
+    app.config["model"] = joblib.load(args.model)
     app.run(debug=True)
--- src/lifecycle.py
+++ src/lifecycle.py
@@ -1,35 +1,44 @@
 """This is full life cycle for ml model"""
+
 import argparse
 
 TRAIN_SIZE = 0.2
-MODEL_NAME = []
+MODEL_NAME = "linear_regression_v2.pkl"
+
 
 def parse_cian():
     """Parse data to data/raw"""
     pass
 
+
 def preprocess_data():
-    """Filter and remove """
+    """Filter and remove"""
     pass
+
 
 def train_model():
+    """Train model and save with MODEL_NAME"""
     pass
+
 
 def test_model():
+    """Test model with new data"""
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Parse arguments and run lifecycle steps"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--split', type=float, 
-                        help='Split data, test relative size, from 0 to 1',
-                        default=TRAIN_SIZE)
-    parser.add_argument('-m', '--model', 
-                        help='Model name', 
-                        default=MODEL_NAME)
+    parser.add_argument(
+        "-s",
+        "--split",
+        type=float,
+        help="Split data, test relative size, from 0 to 1",
+        default=TRAIN_SIZE,
+    )
+    parser.add_argument("-m", "--model", help="Model name", default=MODEL_NAME)
     args = parser.parse_args()
-    
+
     parse_cian()
     preprocess_data()
     train_model()
