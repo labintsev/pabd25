bcf42342d0ef805b7be4e8cdbc1c9bfa89faef1f
labintsev <andrej.labintsev@yandex.ru>
2025-05-07 19:25:37 +0300

    add argument parsing

--- src/lifecycle.py
+++ src/lifecycle.py
@@ -1,4 +1,8 @@
 """This is full life cycle for ml model"""
+import argparse
+
+TRAIN_SIZE = 0.2
+MODEL_NAME = []
 
 def parse_cian():
     """Parse data to data/raw"""
@@ -17,6 +21,15 @@ def test_model():
 
 if __name__ == '__main__':
     """Parse arguments and run lifecycle steps"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--split', type=float, 
+                        help='Split data, test relative size, from 0 to 1',
+                        default=TRAIN_SIZE)
+    parser.add_argument('-m', '--model', 
+                        help='Model name', 
+                        default=MODEL_NAME)
+    args = parser.parse_args()
+    
     parse_cian()
     preprocess_data()
     train_model()
