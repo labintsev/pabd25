b442075c0cd99a5b494667f6b24fcca1adbc6313
labintsev <andrej.labintsev@yandex.ru>
2025-05-07 19:10:10 +0300

    add monolite lifecycle sript

---
+++ src/lifecycle.py
@@ -0,0 +1,23 @@
+"""This is full life cycle for ml model"""
+
+def parse_cian():
+    """Parse data to data/raw"""
+    pass
+
+def preprocess_data():
+    """Filter and remove """
+    pass
+
+def train_model():
+    pass
+
+def test_model():
+    pass
+
+
+if __name__ == '__main__':
+    """Parse arguments and run lifecycle steps"""
+    parse_cian()
+    preprocess_data()
+    train_model()
+    test_model()
